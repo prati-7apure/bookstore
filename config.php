5ac5943eae87e62175b4e9d68e0d86fa66781b4f
prati123456 <113717884+prati123456@users.noreply.github.com>
2024-02-14 00:11:48 +0530

    .

---
+++ config.php
@@ -0,0 +1,6 @@
+
+<?php
+
+// $conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+$conn = mysqli_connect('sql113.infinityfree.com','if0_35938324','cWz3zGXh0reNI','if0_35938324_shop_db') or die('connection failed');
+?>
